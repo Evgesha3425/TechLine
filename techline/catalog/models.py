6f10534f60be5f78c87b072111cb02a452b552ba
Yauheni Eidelman <yauheni3425@gmail.com>
2023-03-11 21:17:33 +0300

    Add new model Goods

--- techline/catalog/models.py
+++ techline/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Goods(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)  # blank=True - поле может быть пустым
+    category = models.SlugField(max_length=20)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')  # upload_to - путь, куда будем сохранять фото, также нужно настроить MEDIA_ROOT и MEDIA_URL
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
--- techline/catalog/urls.py
+++ techline/catalog/urls.py
@@ -1,7 +1,18 @@
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog import views
+from techline import settings
 
 urlpatterns = [
     path('', views.index)
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# В режиме отладки, то есть DEBUG=TRUE, к urlpatterns добавляется еще один маршрут
+# Для статических данных, графических данных указываем сначала settings.MEDIA_URL,
+# а затем корневую папку settings.MEDIA_ROOT где будут находится файлы. Все это делается
+# только в отладочном режиме
